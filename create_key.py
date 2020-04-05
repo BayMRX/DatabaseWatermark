269f1e2e89fa8e9629cbbd5fac9a046491908be5
BayMRX <mujian2009@126.com>
2020-04-05 22:31:38 +0800

    添加随机生成512bit密钥文件的python脚本

--- create_key.py
+++ create_key.py
@@ -1,6 +1,8 @@
 from random import *
+import string
 
-key = "".join([choice("0123456789ABCDEF") for i in range(64)])
+key = "".join([choice(string.ascii_letters + string.digits) for i in range(64)])
+# print(key)
 key_file = open('./key.key', 'w')
 key_file.writelines(key)
 key_file.close()
